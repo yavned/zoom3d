0cbf19396ca312530357f224a67ead7cd8a5350f
Stanislav Sopov <stanislavsopov@192.168.2.77>
2016-10-16 20:01:02 +0200

    Minor refactoring and optimization

--- README.md
+++ README.md
@@ -20,54 +20,54 @@ $(function () {
 ```
 accelerationRatio
 ```
-Higher number produces slower acceleration. Default: ```0.0005```
-     
+Higher number produces slower acceleration. Default: ```0.0005```.
+
 ```       
 content
 ```
-jQuery selector or an element that becomes zoomable/pannable. 
-If no selector is provided, the first child of the plugin element is selected. 
-Default: ```""```
+jQuery selector or an element that becomes zoomable/pannable.
+If no selector is provided, the first child of the plugin element is selected.
+Default: ```""```.
 
 ```
 doubleClickTime
 ```
-Maximum time in milliseconds between two touchend events required for a double 
-tap to occur. 
-Default: ```200```
+Maximum time in milliseconds between two touchend events required for a double
+tap to occur.
+Default: ```200```.
 
 ```
 bounceLimit
 ```
-Distance in pixels within which the content can exceed its parent before it 
-bounces back. 
-Default: ```100```
-       
-``` 
+Distance in pixels within which the content can exceed its parent before it
+bounces back.
+Default: ```100```.
+
+```
 maxScale
 ```
-Maximum scale. Default: ```2```
+Maximum scale. Default: ```2```.
 
 ```
 midScale
 ```
-Medium scale. Used after a double tap. Default: ```1```
+Medium scale. Used after a double tap. Default: ```1```.
 
 ```
 momentumTime
 ```
-Maximum time between touchstart and touchend events required for momentum 
+Maximum time between touchstart and touchend events required for momentum
 panning to occur.
-Default: ```200```
+Default: ```200```.
 
 ```
 translate3d
 ```
-Boolean. When ```true```, the plugin uses 3d transforms to help software 
-acceleration. Default: ```false```
+Boolean. When ```true```, the plugin uses 3d transforms to help software
+acceleration. Default: ```false```.
 
 ```
 onScale
-``` 
-Function called when the content scale changes. The function is passed the 
+```
+Function called when the content scale changes. The function is passed the
 scale.
--- js/jquery.zoom3d.js
+++ js/jquery.zoom3d.js
@@ -5,29 +5,29 @@
  * Licensed under the MIT license
  */
 
-// the semi-colon before the function invocation is a safety 
-// net against concatenated scripts and/or other plugins 
+// the semi-colon before the function invocation is a safety
+// net against concatenated scripts and/or other plugins
 // that are not closed properly.
 ;(function ($, window, document, undefined) {
-    
-    // undefined is used here as the undefined global 
-    // variable in ECMAScript 3 and is mutable (i.e. it can 
-    // be changed by someone else). undefined isn't really 
-    // being passed in so we can ensure that its value is 
-    // truly undefined. In ES5, undefined can no longer be 
+
+    // undefined is used here as the undefined global
+    // variable in ECMAScript 3 and is mutable (i.e. it can
+    // be changed by someone else). undefined isn't really
+    // being passed in so we can ensure that its value is
+    // truly undefined. In ES5, undefined can no longer be
     // modified.
-    
-    // window and document are passed through as local 
-    // variables rather than as globals, because this (slightly) 
-    // quickens the resolution process and can be more 
-    // efficiently minified (especially when both are 
+
+    // window and document are passed through as local
+    // variables rather than as globals, because this (slightly)
+    // quickens the resolution process and can be more
+    // efficiently minified (especially when both are
     // regularly referenced in your plugin).
 
     // Create the defaults once
     var pluginName = "zoom3d",
         defaults = {
             accelerationRatio: 0.0005,
-            content: "", 
+            content: "",
             doubleClickTime: 200,
             bounceLimit: 100,
             maxScale: 2,
@@ -38,18 +38,18 @@
 
     // The actual plugin constructor
     function Plugin(element, options) {
-        this.element = element;
+        this.element = $(element);
 
-        // jQuery has an extend method that merges the 
-        // contents of two or more objects, storing the 
-        // result in the first object. The first object 
-        // is generally empty because we don't want to alter 
+        // jQuery has an extend method that merges the
+        // contents of two or more objects, storing the
+        // result in the first object. The first object
+        // is generally empty because we don't want to alter
         // the default options for future instances of the plugin
         this.options = $.extend({}, defaults, options);
-        
+
         this._defaults = defaults;
         this._name = pluginName;
-                
+
         this.gestureX = 0;
         this.gestureY = 0;
         this.height = 0;
@@ -72,16 +72,16 @@
         this.width = 0;
         this.x = 0;
         this.y = 0;
-        
-        var $element = $(this.element);
-        
-        this.content = this.options.content ? 
-                       $element.find(this.options.content)[0] : 
-                       $element.children().first()[0];
-        
+
+        var $element = this.element;
+
+        this.content = this.options.content ?
+                       $element.find(this.options.content).first() :
+                       $element.children().first();
+
         // Detect touch support
         this.touchEnabled = "ontouchstart" in window;
-        
+
         // Event constants
         this.events = {
             DOUBLE_CLICK: "dblclick",
@@ -97,28 +97,28 @@
 
         this.init();
     }
-    
+
     Plugin.prototype.touchStart = function (event) {
         event.preventDefault();
-        
+
         this.stopTransition();
-        
+
         var self = this;
-        var $element = $(this.element);
-        var $content = $(this.content);
-        
+        var $element = this.element;
+        var $content = this.content;
+
         this.isMoved = false;
 
         this.touchStartTime = event.timeStamp;
 
         var touches = event.originalEvent.touches;
-        
+
         // If it's a two-finger gesture, get its center point
-        if (this.touchEnabled && touches.length === 2) {
+        if (this.touchEnabled && touches.length > 1) {
             this.gestureX = Math.abs(touches[0].pageX + touches[1].pageX) / 2;
             this.gestureY = Math.abs(touches[0].pageY + touches[1].pageY) / 2;
         }
-        
+
         this.touchX = this.touchEnabled ? touches[0].pageX : event.pageX;
         this.touchY = this.touchEnabled ? touches[0].pageY : event.pageY;
 
@@ -126,9 +126,9 @@
         var contentOffset = $content.offset();
         this.x = this.startX = contentOffset.left - elementOffset.left;
         this.y = this.startY = contentOffset.top - elementOffset.top;
-        
-        this.transform(); 
-        
+
+        this.transform();
+
         $content.on(this.events.TOUCH_MOVE, function (event) {
             self.touchMove(event);
         }).on(this.events.TOUCH_END, function (event) {
@@ -137,36 +137,36 @@
     };
 
     Plugin.prototype.touchMove = function (event) {
-        event.preventDefault();  
+        event.preventDefault();
 
         this.isMoved = true;
-        
+
+        var touches = event.originalEvent.touches;
+
         // Ignore if it's a gesture
-        if (this.touchEnabled && event.originalEvent.touches.length > 1) { 
+        if (this.touchEnabled && touches.length > 1) {
             return false;
         }
-        
-        var touches = event.originalEvent.touches;
 
         var pageX = this.touchEnabled ? touches[0].pageX : event.pageX;
         var pageY = this.touchEnabled ? touches[0].pageY : event.pageY;
-        
+
         var deltaX = pageX - this.touchX;
         var deltaY = pageY - this.touchY;
-        
+
         // Slow down if position is out of limits
-        if (this.x < this.minX || this.x > this.maxX) { 
-            deltaX /= 2; 
+        if (this.x < this.minX || this.x > this.maxX) {
+            deltaX /= 2;
         }
-        if (this.y < this.minY || this.y > this.maxY) { 
-            deltaY /= 2; 
+        if (this.y < this.minY || this.y > this.maxY) {
+            deltaY /= 2;
         }
-        
+
         this.x += deltaX;
         this.y += deltaY;
-        
+
         this.transform();
-        
+
         this.touchX = pageX;
         this.touchY = pageY;
     };
@@ -175,17 +175,18 @@
         event.preventDefault();
 
         var touches = event.originalEvent.touches;
-        
+        var $content = this.content;
+
         // Ignore if there are still fingers on screen
         if (this.touchEnabled && touches.length > 0) {
             if (touches.length === 1) {
                 this.touchX = touches[0].pageX;
                 this.touchY = touches[0].pageY;
             }
-            return false; 
+            return false;
         }
-        
-        $(this.content)
+
+        $content
             .off(this.events.TOUCH_MOVE)
             .off(this.events.TOUCH_END);
 
@@ -194,29 +195,30 @@
         if (!this.isMoved) {
             // Simulate dblclick event by timing touch events
             var time = event.timeStamp - this.touchEndTime;
-            if (this.touchEnabled && time <= this.options.doubleClickTime) { 
-                this.toggleZoom(); 
-            } else { 
-                this.update(); 
+            if (this.touchEnabled && time <= this.options.doubleClickTime) {
+                this.toggleZoom();
+            } else {
+                this.update();
             }
             this.touchEndTime = event.timeStamp;
-        } else if (this.touchDuration <= this.options.momentumTime && this.scale > this.minScale) { 
+        } else if (this.touchDuration <= this.options.momentumTime && this.scale > this.minScale) {
             this.applyMomentum();
         } else {
             this.update();
         }
     };
-    
+
     Plugin.prototype.gestureStart = function (event) {
         event.preventDefault();
 
         this.stopTransition();
 
         var self = this;
-        
+        var $content = this.content;
+
         this.startScale = this.scale;
-        
-        $(this.content).on(this.events.GESTURE_CHANGE, function (event) {
+
+        $content.on(this.events.GESTURE_CHANGE, function (event) {
             self.gestureChange(event);
         }).on(this.events.GESTURE_END, function (event) {
             self.gestureEnd(event);
@@ -225,26 +227,28 @@
 
     Plugin.prototype.gestureChange = function (event) {
         event.preventDefault();
-        
+
         var scale = this.startScale * event.originalEvent.scale;
-        
+
         // Slow down if scale is out of limits
-        if (scale < this.minScale) { 
-            scale += (this.minScale - scale) / 2; 
-        } else if (scale > this.maxScale) { 
-            scale += (this.maxScale - scale) / 2; 
+        if (scale < this.minScale) {
+            scale += (this.minScale - scale) / 2;
+        } else if (scale > this.maxScale) {
+            scale += (this.maxScale - scale) / 2;
         }
-        
+
         this.zoom(scale, this.gestureX, this.gestureY);
     };
 
     Plugin.prototype.gestureEnd = function (event) {
         event.preventDefault();
-        
-        $(this.content)
+
+        var $content = this.content;
+
+        $content
             .off(this.events.GESTURE_CHANGE)
             .off(this.events.GESTURE_END);
-        
+
         if (this.scale < this.minScale) {
             this.zoom(this.minScale, this.gestureX, this.gestureY, true);
         } else if (this.scale > this.maxScale) {
@@ -253,127 +257,131 @@
             this.update();
         }
     };
-    
-    Plugin.prototype.applyMomentum = function () {    
+
+    Plugin.prototype.applyMomentum = function () {
         var deltaX = this.x - this.startX;
         var deltaY = this.y - this.startY;
-        var $content = $(this.content);
+        var $content = this.content;
         var self = this;
-        
-        var momentumX = this.getMomentum(this.x, deltaX, this.minX, this.maxX); 
-        var momentumY = this.getMomentum(this.y, deltaY, this.minY, this.maxY); 
-        
+
+        var momentumX = this.getMomentum(this.x, deltaX, this.minX, this.maxX);
+        var momentumY = this.getMomentum(this.y, deltaY, this.minY, this.maxY);
+
         this.x += momentumX.delta;
         this.y += momentumY.delta;
-        
+
         var duration = Math.max(momentumX.duration, momentumY.duration) + "ms";
-        
+
         $content.one(this.events.TRANSITION_END, function() {
             self.update();
         });
-        
+
         $content.css({
             transitionDuration: duration,
             webkitTransitionDuration: duration
         });
-        
+
         self.transform();
     };
 
     Plugin.prototype.getMomentum = function (startVal, distance, minVal, maxVal) {
         var velocity = distance / this.touchDuration;
-        var acceleration = velocity < 0 ? 
-                           this.options.accelerationRatio : 
+        var acceleration = velocity < 0 ?
+                           this.options.accelerationRatio :
                            -this.options.accelerationRatio;
         var delta = -(velocity * velocity) / (2 * acceleration);
         var duration = -velocity / acceleration;
-        
-        // This is built on the premise that minVal is always less than 0, 
+
+        // This is built on the premise that minVal is always less than 0,
         // and maxVal is always greater than 0
         var minDelta = minVal - this.options.bounceLimit - startVal;
-        var maxDelta = maxVal + this.options.bounceLimit - startVal; 
-        
+        var maxDelta = maxVal + this.options.bounceLimit - startVal;
+
         // Adjust delta and time for out-of-boundary values
         if (delta < minDelta || delta > maxDelta) {
-            var relativeDelta = delta < 0 ? 
-                                Math.abs(minDelta / delta) : 
+            var relativeDelta = delta < 0 ?
+                                Math.abs(minDelta / delta) :
                                 Math.abs(maxDelta / delta);
             duration *= relativeDelta;
             delta = Math.max(minDelta, Math.min(delta, maxDelta));
         }
-        
+
         return { delta: delta, duration: duration };
     };
-    
+
     // Make content fit inside parent element
     Plugin.prototype.resize = function () {
-        var elementWidth = $(this.element).width();
-        var elementHeight = $(this.element).height();
-        var contentWidth = $(this.content).outerWidth();
-        var contentHeight = $(this.content).outerHeight();
+        var $element = this.element;
+        var $content = this.content;
+        var elementWidth = $element.width();
+        var elementHeight = $element.height();
+        var contentWidth = $content.outerWidth();
+        var contentHeight = $content.outerHeight();
         var elementRatio = elementWidth / elementHeight;
         var contentRatio = contentWidth / contentHeight;
-        this.scale = this.minScale = elementRatio > contentRatio ? 
-                                     elementHeight / contentHeight : 
+        this.scale = this.minScale = elementRatio > contentRatio ?
+                                     elementHeight / contentHeight :
                                      elementWidth / contentWidth;
 
         this.update();
     };
-    
+
     Plugin.prototype.update = function () {
-        var $element = $(this.element);
-        var $content = $(this.content);
-        
+        var $element = this.element;
+        var $content = this.content;
+
         var elementWidth = $element.width();
-        var elementHeight = $element.height();        
+        var elementHeight = $element.height();
 
         this.width = $content.outerWidth() * this.scale;
         this.height = $content.outerHeight() * this.scale;
-        
-        this.minX = this.width < elementWidth ? 
-                    (elementWidth - this.width) / 2 : 
+
+        this.minX = this.width < elementWidth ?
+                    (elementWidth - this.width) / 2 :
                     elementWidth - this.width;
-        this.minY = this.height < elementHeight ? 
-                    (elementHeight - this.height) / 2 : 
+        this.minY = this.height < elementHeight ?
+                    (elementHeight - this.height) / 2 :
                     elementHeight - this.height;
         this.maxX = this.width < elementWidth ? this.minX : 0;
         this.maxY = this.height < elementHeight ? this.minY : 0;
-        
+
         this.x = Math.max(this.minX, Math.min(this.x, this.maxX));
         this.y = Math.max(this.minY, Math.min(this.y, this.maxY));
-        
+
         $content.css({
             transitionDuration: "200ms",
             webkitTransitionDuration: "200ms"
         });
-        
+
         this.transform();
     };
-    
+
     Plugin.prototype.stopTransition = function () {
-        $(this.content).css({
+        var $content = this.content;
+
+        $content.css({
             transitionDuration: "0ms",
             webkitTransitionDuration: "0ms"
         });
     };
-    
+
     Plugin.prototype.toggleZoom = function () {
-        var scale = this.scale !== this.minScale ? 
-                                   this.minScale : 
+        var scale = this.scale !== this.minScale ?
+                                   this.minScale :
                                    this.midScale;
         this.zoom(scale, this.touchX, this.touchY, true);
     };
-    
+
     Plugin.prototype.zoom = function (scale, centerX, centerY, once) {
         var relativeScale = scale / this.scale;
 
         var elementOffset = $(this.element).offset();
         var x = centerX - this.x - elementOffset.left;
         var y = centerY - this.y - elementOffset.top;
-        
+
         this.x = x - x * relativeScale + this.x;
         this.y = y - y * relativeScale + this.y;
-        
+
         this.scale = scale;
 
         if (once) {
@@ -382,41 +390,43 @@
             this.transform();
         }
     };
-    
+
     Plugin.prototype.transform = function () {
+        var $content = this.content;
+
         var transform = (this.options.translate3d ? "translate3d(" : "translate(") +
-                        this.x.toFixed(2) + "px," + 
-                        this.y.toFixed(2) + "px" +    
-                        (this.options.translate3d ? ",0) " : ") ") + 
-                        "scale(" + this.scale.toFixed(2) + ")";
-        
-        $(this.content).css({
+                        this.x + "px," +
+                        this.y + "px" +
+                        (this.options.translate3d ? ",0) " : ") ") +
+                        "scale(" + this.scale + ")";
+
+        $content.css({
             transform: transform,
             webkitTransform: transform
         });
-        
+
         if (this.options.onScale && typeof this.options.onScale === "function") {
             this.options.onScale(this.scale);
         }
     };
-    
+
     Plugin.prototype.init = function () {
         // Place initialization logic here
         // You already have access to the DOM element and
-        // the options via the instance, e.g. this.element 
+        // the options via the instance, e.g. this.element
         // and this.options
-        
+
         var self = this;
-        var $content = $(this.content);
-        
+        var $content = this.content;
+
         $content.on(this.events.TOUCH_START, function (event) {
             self.touchStart(event);
         }).on(this.events.GESTURE_START, function (event) {
-            self.gestureStart(event);   
+            self.gestureStart(event);
         }).on(this.events.DOUBLE_CLICK, function () {
-            self.toggleZoom();  
+            self.toggleZoom();
         });
-        
+
         $(window).bind(this.events.RESIZE, function () {
             self.resize();
         });
@@ -424,7 +434,7 @@
         this.resize();
     };
 
-    // A really lightweight plugin wrapper around the constructor, 
+    // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
     $.fn[pluginName] = function (options) {
         return this.each(function () {
